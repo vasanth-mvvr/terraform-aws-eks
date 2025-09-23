77a5fffe630cbf7fcd0de78826a8853cde8fe060
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-09-23 11:28:08 +0530

    latest

--- 00-vpc/variables.tf
+++ 00-vpc/variables.tf
@@ -1,3 +1,31 @@
 variable "project" {
   default = "expense"
 }
+
+variable "public_subnet" {
+  type = list
+  default = ["10.0.1.0/24","10.0.2.0/24"]
+}
+
+variable "private_subnet" {
+  type = list
+  default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+variable "database_subnet" {
+  type = list
+  default = ["10.0.21.0/24","10.0.22.0/24"]
+}
+
+variable "common_tag" {
+  type = map
+  default = {
+    Name = "expense"
+    environment = "dev"
+  }
+
+}
+
+variable "is_peering_required" {
+  type = bool
+  default = true
+}
--- 00-vpc/vpc.tf
+++ 00-vpc/vpc.tf
@@ -1,4 +1,9 @@
 module "vpc" {
   source = "git::https://github.com/vasanth-mvvr/expense-aws-terraform.git?ref=main"
   project = var.project
+  common_tags = var.common_tag
+  public_subnet_cidrs = var.public_subnet
+  private_subnet_cidrs = var.private_subnet
+  database_subnet_cidrs = var.database_subnet
+  is_peering_required = var.is_peering_required
 }
