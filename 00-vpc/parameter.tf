037daadc1739071f7a201318aeff97e0f4da9ded
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-09-23 11:30:08 +0530

    latest

--- 00-vpc/parameter.tf
+++ 00-vpc/parameter.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project}/${var.common_tags.environment}/vpc_id"
+  name = "/${var.project}/${var.common_tag.environment}/vpc_id"
   type = String
   value = module.vpc.vpc_id
 }
 
 resource "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project}/${var.common_tags.environment}/public_subnet_ids"
+  name = "/${var.project}/${var.common_tag.environment}/public_subnet_ids"
   type = StringList
   value = join(",",module.vpc.public_subnet_ids)
 }
